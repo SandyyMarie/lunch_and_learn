aa7bd343f0b103c40c68e24b186bbddec5a7902d
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-14 11:29:27 -0700

    feat+test: added additional methods to random_country facade to also return coords for places API

--- app/facades/random_country_facade.rb
+++ app/facades/random_country_facade.rb
@@ -2,6 +2,15 @@ class RandomCountryFacade
   def self.country_randomizer
     country_options = RandomCountryService.country_list
     chosen_country = country_options.sample
-    chosen_country[:name][:common]
+
+    # chosen_country[:name][:common]
+  end
+
+  def self.random_country_name
+    country_randomizer[:name][:common]
+  end
+
+  def self.random_country_lat_long
+    country_randomizer[:latlng]
   end
 end
--- spec/facades/random_country_facade_spec.rb
+++ spec/facades/random_country_facade_spec.rb
@@ -3,8 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Random Country Facade', :vcr do
   it '#country_randomizer' do
     country = RandomCountryFacade.country_randomizer
+    expect(country).to be_a(Hash)
+  end
 
+  it '#random_country_name' do
+    country = RandomCountryFacade.random_country_name
     expect(country).to be_an(String)
   end
 
+  it '#random_country_lat_long' do
+    country_coords = RandomCountryFacade.random_country_lat_long
+    expect(country_coords).to be_a(Array)
+  end
 end
