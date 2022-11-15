1b7237fe417ad6738ad6fde86de7a9da127521d5
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-15 11:40:57 -0700

    refactor: updated error message

--- app/controllers/api/v1/air_quality_controller.rb
+++ app/controllers/api/v1/air_quality_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::AirQualityController < ApplicationController
     if RandomCountryFacade.country_exists?(params[:country])
       render json: AirQualitySerializer.new(AirQualityFacade.air_quality_for_country(params[:country]))
     else
-      render json: {error: "Please check the spelling of Country, #{params[:country]} does not exist"}, status: 400
+      render json: {error: "Please check the spelling of Country, '#{params[:country]}' is not valid"}, status: 400
     end
   end
 end
