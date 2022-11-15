88f19e61a4be4feb59731f9a705d65e2dc8b00cb
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-15 11:25:05 -0700

    feat: all tests passing, json being appropriately rendered with expected response

--- app/controllers/api/v1/air_quality_controller.rb
+++ app/controllers/api/v1/air_quality_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::AirQualityController < ApplicationController
   def index
+
+    render json: AirQualitySerializer.new(AirQualityFacade.air_quality_for_country(params[:country]))
   end
 end
