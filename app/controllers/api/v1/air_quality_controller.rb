92c15ea3f2dbc36a4bddabe385f7657293d59f9c
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-15 11:39:44 -0700

    feat: added error handling for counteries that don't exist or are spelled incorrectly

--- app/controllers/api/v1/air_quality_controller.rb
+++ app/controllers/api/v1/air_quality_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::AirQualityController < ApplicationController
   def index
-
-    render json: AirQualitySerializer.new(AirQualityFacade.air_quality_for_country(params[:country]))
+    if RandomCountryFacade.country_exists?(params[:country])
+      render json: AirQualitySerializer.new(AirQualityFacade.air_quality_for_country(params[:country]))
+    else
+      render json: {error: "Please check the spelling of Country, #{params[:country]} does not exist"}, status: 400
+    end
   end
 end
