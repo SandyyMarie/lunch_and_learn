593cef6b063e054880f5bf715e11fbc41a0b6e95
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-15 01:31:17 -0700

    feat: added edge casing to check for empty country param to trigger randomizer

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::RecipesController < ApplicationController
   def index
-    render json: RecipeSerializer.new(RecipeFacade.get_recipes_for_country(params[:country]))
+
+    if params[:country] == nill
+      search_country == RandomCountryFacade.random_country_name
+    else
+      search_country == params[:country]
+    end
+
+    render json: RecipeSerializer.new(RecipeFacade.get_recipes_for_country(search_country))
   end
 end
--- spec/facades/random_country_facade_spec.rb
+++ spec/facades/random_country_facade_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Random Country Facade', :vcr do
-  it '#country_randomizer' do
-    country = RandomCountryFacade.country_randomizer
+    it '#country_randomizer' do
+      country = RandomCountryFacade.country_randomizer
+      expect(country).to be_a(Hash)
+    end
 
-    expect(country).to be_an(String)
-  end
+    it '#random_country_name' do
+      country = RandomCountryFacade.random_country_name
+      expect(country).to be_an(String)
+    end
 
+    it '#random_country_lat_long' do
+      country_coords = RandomCountryFacade.random_country_lat_long
+      expect(country_coords).to be_a(Array)
+    end
+
+    it '#specific_country_latlong' do
+      lat_lng = RandomCountryFacade.specific_country_latlong("New Zealand")
+      expect(lat_lng).to be_a(Array) #might need to update
+    end
 end
