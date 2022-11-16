729206a4d2acff3ce0086d06817c241a07057ebf
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-15 18:19:26 -0700

    feat: added param to render empty json data set for empty string in country param

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -5,6 +5,8 @@ class Api::V1::RecipesController < ApplicationController
        search_country = RandomCountryFacade.random_country_name
     elsif RandomCountryFacade.country_exists?(params[:country])
       search_country = params[:country]
+    elsif params[:country] == ""
+      render json: {data: []}
     else #might need to move render json into within block if it doesnt break before getting there
       render json: {error: "Please check the spelling of Country, '#{params[:country]}' is not valid"}, status: 400
     end
