4d0efd31e8244947c5e61870ca4cc85224c30017
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-16 05:14:23 -0700

    test: added model validation testing for user and favorites including relationship testing

---
+++ spec/models/favorites_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+ RSpec.describe Favorites, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:country) }
+    it { should validate_presence_of(:recipe_link) }
+    it { should validate_presence_of(:recipe_title) }
+  end
+
+  describe 'relationships' do
+    it {should belong_to :user}
+  end
+
+end
