7c7e499885ce3aba13811e9884cd5cfdd8c03368
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-15 18:02:19 -0700

    refactor: seperated the recipe facade call within the serializer per CICD reccomendations

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::RecipesController < ApplicationController
     else #might need to move render json into within block if it doesnt break before getting there
       render json: {error: "Please check the spelling of Country, '#{params[:country]}' is not valid"}, status: 400
     end
-
-    render json: RecipeSerializer.new(RecipeFacade.get_recipes_for_country(search_country))
+    data = RecipeFacade.get_recipes_for_country(search_country)
+    render json: RecipeSerializer.new(data)
   end
 end
