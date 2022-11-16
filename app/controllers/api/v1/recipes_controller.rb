b716bbfc0b640edebb553394830c78e35bb390d7
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-15 23:08:28 -0700

    refactor: refactored recipes controller to have better render flow, all tests passing except routing for learning_resources

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,16 +1,36 @@
 class Api::V1::RecipesController < ApplicationController
   def index
 
-    if params[:country] == nil
-       search_country = RandomCountryFacade.random_country_name
-    elsif RandomCountryFacade.country_exists?(params[:country])
-      search_country = params[:country]
-    elsif params[:country] == ""
+    if params[:country] == nil ##if no param sent, randomize
+      search_country = RandomCountryFacade.random_country_name
+      data = RecipeFacade.get_recipes_for_country(search_country)
+      render json: RecipeSerializer.new(data)
+    elsif params[:country] == "" ##if param is empty string, return empty data set
       render json: {data: []}
-    else #might need to move render json into within block if it doesnt break before getting there
+    elsif RandomCountryFacade.country_exists?(params[:country].titleize) ##if country param sent AND valid
+      search_country = params[:country]
+      data = RecipeFacade.get_recipes_for_country(search_country)
+      render json: RecipeSerializer.new(data)
+    else ## else country param WAS sent but was NOT a valid country
       render json: {error: "Please check the spelling of Country, '#{params[:country]}' is not valid"}, status: 400
     end
-    data = RecipeFacade.get_recipes_for_country(search_country)
-    render json: RecipeSerializer.new(data)
+
+    # data = RecipeFacade.get_recipes_for_country(search_country)
+    # render json: RecipeSerializer.new(data)
+    # #####
+    # if params[:country] == nil
+    #   search_country = RandomCountryFacade.random_country_name
+    #   data = RecipeFacade.get_recipes_for_country(search_country)
+    #   render json: RecipeSerializer.new(data)
+    # elsif RandomCountryFacade.country_exists?(params[:country])
+    #   search_country = params[:country]
+    #   data = RecipeFacade.get_recipes_for_country(search_country)
+    #   render json: RecipeSerializer.new(data)
+    # elsif params[:country] == ""
+    #   render json: {data: []}
+    # else #might need to move render json into within block if it doesnt break before getting there
+    #   render json: {error: "Please check the spelling of Country, '#{params[:country]}' is not valid"}, status: 400
+    # end
+
   end
 end
