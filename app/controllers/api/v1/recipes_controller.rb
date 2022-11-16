b4d5b2b6489f0fcda70defc6a56ddc57d56911c2
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-15 23:15:57 -0700

    feat: learning resources route and controller with test passing

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::RecipesController < ApplicationController
   def index
-
     if params[:country] == nil ##if no param sent, randomize
       search_country = RandomCountryFacade.random_country_name
       data = RecipeFacade.get_recipes_for_country(search_country)
@@ -14,23 +13,5 @@ class Api::V1::RecipesController < ApplicationController
     else ## else country param WAS sent but was NOT a valid country
       render json: {error: "Please check the spelling of Country, '#{params[:country]}' is not valid"}, status: 400
     end
-
-    # data = RecipeFacade.get_recipes_for_country(search_country)
-    # render json: RecipeSerializer.new(data)
-    # #####
-    # if params[:country] == nil
-    #   search_country = RandomCountryFacade.random_country_name
-    #   data = RecipeFacade.get_recipes_for_country(search_country)
-    #   render json: RecipeSerializer.new(data)
-    # elsif RandomCountryFacade.country_exists?(params[:country])
-    #   search_country = params[:country]
-    #   data = RecipeFacade.get_recipes_for_country(search_country)
-    #   render json: RecipeSerializer.new(data)
-    # elsif params[:country] == ""
-    #   render json: {data: []}
-    # else #might need to move render json into within block if it doesnt break before getting there
-    #   render json: {error: "Please check the spelling of Country, '#{params[:country]}' is not valid"}, status: 400
-    # end
-
   end
 end
