432feaeb32043f7ee1424d4b7c3c03724dabd74b
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-15 17:10:25 -0700

    refactor: added params regarding country search for recipes

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::RecipesController < ApplicationController
   def index
 
-    if params[:country] == nill
-      search_country == RandomCountryFacade.random_country_name
-    else
-      search_country == params[:country]
+    if params[:country] == nil
+       search_country = RandomCountryFacade.random_country_name
+    elsif RandomCountryFacade.country_exists?(params[:country])
+      search_country = params[:country]
+    else #might need to move render json into within block if it doesnt break before getting there
+      render json: {error: "Please check the spelling of Country, '#{params[:country]}' is not valid"}, status: 400
     end
 
     render json: RecipeSerializer.new(RecipeFacade.get_recipes_for_country(search_country))
