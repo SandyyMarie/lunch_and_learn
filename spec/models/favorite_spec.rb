8c34f112191f462717c9582affb0dbe346fe8a33
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-16 05:35:40 -0700

    feat+test: added favorite table, model, route, and controller with error handling for no user with given api_key

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
- RSpec.describe Favorites, type: :model do
+ RSpec.describe Favorite, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:country) }
     it { should validate_presence_of(:recipe_link) }
